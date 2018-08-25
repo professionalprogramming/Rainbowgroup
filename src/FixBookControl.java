dbcd66744323b199252f4d1fecf293daa4ea4b15
professionalprogramming <41613977+professionalprogramming@users.noreply.github.com>
2018-08-25 21:12:01 +1000

    Update FixBookControl.java
    this code review by kumaran there are no errors in this file

--- src/FixBookControl.java
+++ src/FixBookControl.java
@@ -1,5 +1,5 @@
 public class FixBookControl {
-	////this code review by kumaran there are no errors in this file
+	//this code review by kumaran there are no errors in this file
 	private FixBookUI ui;
 	private Enum CONTROL_STATE { INITIALISED, READY, FIXING }; //enum to Enum by Author 
 	private CONTROL_STATE state;
