8e5c68e6e0fc4a98d877fab038d1e20cd223c234
kumaran24 <41821468+kumaran24@users.noreply.github.com>
2018-08-26 19:52:22 +1000

    Update member.java

--- src/member.java
+++ src/member.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 //this code review by kumaran there are no errors in this file
+// this file Moderator by kumaran there are no conflicts 
 @SuppressWarnings("serial")
 public class Member implements Serializable { //class name should be caps
 
